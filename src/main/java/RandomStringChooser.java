cfab92280347ad68db76075400c187bb81633c19
notifrin <80722932+notifrin@users.noreply.github.com>
2025-04-25 23:54:47 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,12 +1,31 @@
 import java.util.*;
 public class RandomStringChooser
 {
-private ArrayList<String> valuesRemaining;
+ private String[] values;
+    private int valuesRemaining;
 
-    public RandomStringChooser(String[] values)
+    public RandomStringChooser(String[] vals)
     {
-        valuesRemaining = new ArrayList<String>();
+        values = new String[vals.length];
 
-        for(String value : values)
-            valuesRemaining.add(value);
+        for(int i = 0; i < values.length; i++)
+            values[i] = vals[i];
+
+        valuesRemaining = values.length;
+    }
+
+    public String getNext()
+    {
+        if(valuesRemaining == 0)
+            return "NONE";
+
+        int index = (int) (Math.random() * valuesRemaining);
+
+        String selected = values[index];
+        values[index] = values[valuesRemaining - 1];
+
+        valuesRemaining--;
+
+        return selected;
+    }
 }
